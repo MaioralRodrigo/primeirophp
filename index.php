f4ac922d9ff5b6e02f5bdc748bfafc06d59596db
RodrigoMaioral <maioralrodrigo@gmail.com>
2021-02-02 11:10:19 -0700

    Site Blog

--- index.php
+++ index.php
@@ -59,9 +59,9 @@
 
       <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
         <div class="col-md-6 px-0">
-          <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
-          <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
-          <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
+          <h1 class="display-4 font-italic">Rodrigo Maioral</h1>
+          <p class="lead my-3">Ola, sou Rodrigo e tenho a missao de aprender tudo o que esta sendo ensinado no curso gama</p>
+          <p class="lead mb-0"><a target="_blank" href="https://whats.link/testecurso14c" class="text-white font-weight-bold">Clique aqui para enviar uma mensagem por Whatsapp</a></p>
         </div>
       </div>
 
