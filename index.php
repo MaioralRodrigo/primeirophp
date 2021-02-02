77a8c739e4ad6e7285899993ddcba1b48d507480
RodrigoMaioral <maioralrodrigo@gmail.com>
2021-02-02 10:20:54 -0700

    teste VS Code

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <html>
 <h1> Teste </h1>
-<h1> Teste de edicao no Git </h1>
+<h1> Agora por dentro do VS Code </h1>
+
+<div style="display:flex; height: 400px">
+  <div style="backgroud-color:blue">
+    Essa e uma coluna azul
+  </div>
+</div>
+
+
+
 </html>
