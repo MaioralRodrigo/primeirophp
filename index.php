ca9d0decca5078c3eedacad61a63e2d91b3c7afa
RodrigoMaioral <maioralrodrigo@gmail.com>
2021-02-02 10:30:58 -0700

    Teste 2

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <h1> Teste </h1>
 <h1> Agora por dentro do VS Code </h1>
 
-<div style="display:flex; height: 400px">
+<div style="display:flex; height:400px">
   <div style="backgroud-color:blue">
     Essa e uma coluna azul
   </div>
+  <div style="backgroud-color:yellow">
+    Essa e uma coluna azul
+  </div>
 </div>
-
-
-
 </html>
